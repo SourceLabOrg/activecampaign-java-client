d091c94fa03d4253a6a959c0eab4d3d2d91bf240
Crim <stephen.powis@gmail.com>
2019-12-02 19:02:34 +0900

    cleanup

--- src/main/java/org/sourcelab/activecampaign/request/LoginRequest.java
+++ src/main/java/org/sourcelab/activecampaign/request/LoginRequest.java
@@ -1,3 +1,20 @@
+/**
+ * Copyright 2019 SourceLab.org https://github.com/SourceLabOrg/activecampaign-java-client
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+ * persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package org.sourcelab.activecampaign.request;
 
 import org.slf4j.Logger;
@@ -29,7 +46,7 @@ public class LoginRequest implements Request<Boolean> {
     }
 
     @Override
-    public Boolean parseResponse(final String s) throws IOException {
+    public Boolean parseResponse(final String response) throws IOException {
         // If the request is valid, we just return true.
         // If the request is invalid, we'll throw an exception.
         return true;
